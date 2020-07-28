bec68bb41bfaa03b3e4c42987bc578838d4a1c1d
Administrator <2645978312@qq.com>
2020-07-28 16:03:05 +0800

    本低dev分支提交文件

--- Test1/src/org/sang/bean/Cat.java
+++ Test1/src/org/sang/bean/Cat.java
@@ -1,6 +1,22 @@
 package org.sang.bean;
 
 public class Cat {
+	public int getCatId() {
+		return catId;
+	}
+	public void setCatId(int catId) {
+		this.catId = catId;
+	}
+	public String getName() {
+		return name;
+	}
+	@Override
+	public String toString() {
+		return "Cat [catId=" + catId + ", name=" + name + "]";
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
 	private int catId;
 	private String name;
 }
